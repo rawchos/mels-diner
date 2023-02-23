86b6e2b95a380f9861181cec4e1f5427f89b368d
Neal Scott <nealnmel@gmail.com>
2023-02-23 14:24:28 -0600

    Minor updates to allow easily running from the repl

--- src/mels_diner/core.clj
+++ src/mels_diner/core.clj
@@ -11,14 +11,29 @@
 ;;       Also, be aware of when program execution stops and if we still have
 ;;       go processes in flight. Might need a check in here that's waiting for
 ;;       kitchen shutdown notification.
-(defn -main
+(defn run-simulation
   "Kicks off the orders simulation. It takes in a file of simulation orders or
    defaults to `resources/small-orders.json` if nothing specified. The kitchen
    is configured based off the `resources/config.edn` file."
+  ([] (run-simulation "resources/small-orders.json"))
+  ([orders-file]
+   (let [config (-> (cfg/get-config)
+                    (assoc :orders-file (or orders-file
+                                            "resources/small-orders.json")))]
+     (log/infof "Running with the following config: %s" config)
+     (kitchen/receive-orders)
+     (kitchen/simulate-orders config))))
+
+(defn -main
+  "Entrypoint for application when run from the command line. Defers application
+   execution to `run-simulation`."
   [& [orders-file]]
-  (let [config (-> (cfg/get-config)
-                   (assoc :orders-file (or orders-file
-                                           "resources/small-orders.json")))]
-    (log/infof "Running with the following config: %s" config)
-    (kitchen/receive-orders)
-    (kitchen/simulate-orders config)))
+  (run-simulation orders-file))
+
+(comment
+  ;; Run from the repl with default orders file:
+  (run-simulation)
+  
+  ;; Run from the repl with specified orders file:
+  (run-simulation "resources/small-orders.json")
+  )
