c8e6d72a1a2d32bd52810193b6bdf605ed1d8d1a
Neal Scott <nealnmel@gmail.com>
2023-02-26 14:10:23 -0600

    Remove nested doseq on simulate-orders

--- src/mels_diner/kitchen.clj
+++ src/mels_diner/kitchen.clj
@@ -32,19 +32,19 @@
 (defn seconds->millis [seconds]
   (* seconds 1000))
 
-;; TODO: I'm not sure I like the nested doseq. Possibly revisit but good enough
-;;       to continue forward for now.
 (defn simulate-orders
   "Simulates receiving orders. Order intake rate and file containing simulated
    orders are passed in the config."
   [{:keys [ingestion-count ingestion-rate orders-file]}]
   (log/infof "Placing orders from [%s] at an ingestion rate of %d orders every %d seconds"
              orders-file ingestion-count ingestion-rate)
-  (doseq [orders (->> (util/load-orders orders-file)
-                      (partition-all ingestion-count))]
-    (doseq [order orders]
-      (go (>! orders-intake order)))
-    (<!! (timeout (seconds->millis ingestion-rate))))
+  (loop [all-orders (->> (util/load-orders orders-file)
+                         (partition-all ingestion-count))]
+    (when-let [orders (first all-orders)]
+      (doseq [order orders]
+        (go (>! orders-intake order)))
+      (<!! (timeout (seconds->millis ingestion-rate)))
+      (recur (rest all-orders))))
   (close! orders-intake))
 
 (defn same-order?
